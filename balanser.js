42103d88bb7e68a685401c65bad04c148d204143
Alex <lxknvlk@gmail.com>
2019-06-13 14:33:59 +0300

    asd

--- balanser.js
+++ balanser.js
@@ -5,48 +5,46 @@ const bodyParser = require('body-parser'); //npm install request-promise
 const request = require("request-promise");
 const port = 8000
 
-var servers = [8001, 8002, 8003, 8004]
+var servers = [8001, 8002, 8003, 8004, 8005, 8006, 8007, 8008]
 
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(bodyParser.json({ extended: true, limit: '10mb' }));
+app.use(express.methodOverride());
 
 var processingRequest = false;
 var requestProcessed = false;
 
 app.post('/', (req, res) => {
-	console.log("processing post request in balanser");
+	//console.log("processing post request in balanser");
 	requestProcessed = false;
-	processingRequest = false;
 	handleRequest(req, res);
 })
 
 async function handleRequest(req, res){
-	while (!requestProcessed){
-		if (!processingRequest){
-			processingRequest = true;
-			for(var i = 0; i < servers.length; i++){
-				servport = servers[i];
-				console.log("checking server " + servport);
-				var statusRes = await checkServerStatus(servport);
-				var available = parseInt(statusRes)
-				console.log("server on port "+ servport + " available: " + available);
+	while(!requestProcessed){
+		startInstance = getRandomInt(9); //servercount + 1 
+		for(var i = startInstance; i < servers.length; i++){
+			servport = servers[i];
+			//console.log("checking server " + servport);
+			var statusRes = await checkServerStatus(servport); //takes <20ms
+			var available = parseInt(statusRes)
+			//console.log("server on port "+ servport + " available: " + available);
 
-				if (available == 1){
-					console.log("forwarding request to server " + servport);
-					var classifyResult = await sendImageToServer(servport, req.body)
-					console.log("response from server " + servport + ": " + classifyResult);
-					requestProcessed = true;
-					processingRequest = false;
-					res.write(classifyResult.toString());
-					res.status(200);
-					res.end(); 
-					return;
-				}
+			if (available == 1){
+				console.log("forwarding request to server " + servport);
+				var classifyResult = await sendImageToServer(servport, req.body)
+				//console.log("response from server " + servport + ": " + classifyResult);
+				requestProcessed = true;
+				res.write(classifyResult.toString());
+				res.status(200);
+				res.end(); 
+				return;
 			}
-		}	
+		}
 
+		//setTimeout(handleRequest(req, res), 50);
+		await sleep(50)
 		console.log("restarting server checks");
-		setTimeout(handleRequest(req, res), 3000);
 	}
 }
 
@@ -58,6 +56,12 @@ async function checkServerStatus(servport){
 	return await request.get('http://localhost:' + servport);
 }
 
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
+
 async function sendImageToServer(servport, reqbody){
 	return await request.post({
 		url: 'http://localhost:' + servport,
@@ -66,10 +70,8 @@ async function sendImageToServer(servport, reqbody){
 	});
 }
 
-function sleep(ms){
-    return new Promise(resolve=>{
-        setTimeout(resolve,ms)
-    })
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 app.listen(port, (err) => {
