56775ad14aed728e57082d38ff8772f865209e54
Alex <lxknvlk@gmail.com>
2019-06-13 13:41:12 +0300

    calling python in async

--- balanser.js
+++ balanser.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-
-var bodyParser = require('body-parser');
-var request = require('request'); //npm install request
+const bodyParser = require('body-parser'); //npm install request-promise
+// const request = require('request'); //npm install request
+const request = require("request-promise");
 const port = 8000
 
 var servers = [8001, 8002, 8003, 8004]
@@ -10,62 +10,69 @@ var servers = [8001, 8002, 8003, 8004]
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(bodyParser.json({ extended: true, limit: '10mb' }));
 
+var processingRequest = false;
+var requestProcessed = false;
+
 app.post('/', (req, res) => {
 	console.log("processing post request in balanser");
-	//req.body.image or just forward req
-
-	// var processed = false;
-	// while (!processed){
-	// 	for (servers){
-	// 		if (server.idle) call function and return result and set processed = true
-	// 	}
-	// 	pause 150
-	// }
 	requestProcessed = false;
-	servport = 8001;
-
-	checkServerStatus(servport, function(response){
-		console.log("server on port "+ servport + " available: " + response);
-		available = parseInt(response)
-
-		if (available == 1){
-			console.log("forwarding request to server " + servport);
-			sendImageToServer(servport, req.body, function(response){
-				console.log("response from server " + servport + " :" + response);
-				res.write(response.toString());
-				res.status(200);
-				res.end(); 
-			})
-		} else {
-			res.status(200);
-			res.end(); 
-		}
-	});
+	processingRequest = false;
+	handleRequest(req, res);
+	console.log("post request processed!");
 })
+
+async function handleRequest(res, req){
+	while (!requestProcessed){
+		if (!processingRequest){
+			processingRequest = true;
+			console.log("processing request in while/if");
+			for(var i = 0; i < servers.length; i++){
+				servport = servers[i];
+				console.log("checking server " + servport);
+				var statusRes = await checkServerStatus(servport);
+				var available = parseInt(statusRes)
+				console.log("server on port "+ servport + " available: " + available);
+				
+				if (available == 1){
+					console.log("forwarding request to server " + servport);
+					var classifyResult = await sendImageToServer(servport, req.body)
+					console.log("response from server " + servport + " :" + classifyResult);
+					requestProcessed = true;
+					processingRequest = false;
+					res.write(response.toString());
+					res.status(200);
+					res.end(); 
+					return;
+				}
+			}
+		}	
+
+		console.log("restarting server checks");
+		setTimeout(handleRequest(res, req), 3000);
+	}
+}
 
 app.use(function(err, req, res, next) {
       console.error("an error occured " + err);
 })
 
-function checkServerStatus(servport, callback){
-	request.get('http://localhost:' + servport, function(err, res, body){
-		if(err) console.log("error from python: " + err);
-		callback(body);
-	});
+async function checkServerStatus(servport){
+	return await request.get('http://localhost:' + servport);
 }
 
-function sendImageToServer(servport, reqbody, callback){
-	request.post({
+async function sendImageToServer(servport, reqbody){
+	return await request.post({
 		url: 'http://localhost:' + servport,
 		body: reqbody,
 		json: true
-	}, function(error, response, body){
-        if (error) console.log("error from processing image: " +  error);
-        //if (body) console.log("body from python: " + body);
-        callback(body);
 	});
 }
 
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
 
 app.listen(port, (err) => {
 	if (err) console.log('something bad happened', err);
